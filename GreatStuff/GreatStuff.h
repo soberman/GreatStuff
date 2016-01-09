3e13151fcd7c89128e96f6020ec4fd3b60e3bab7
Yaroslav Arsenkin <majesticy@gmail.com>
2016-01-09 20:50:06 +0100

    Adds the header file of the NSURL category to make it visible in the exported framework.

--- GreatStuff/GreatStuff.h
+++ GreatStuff/GreatStuff.h
@@ -16,5 +16,5 @@ FOUNDATION_EXPORT double GreatStuffVersionNumber;
 FOUNDATION_EXPORT const unsigned char GreatStuffVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <GreatStuff/PublicHeader.h>
-
+#import <GreatStuff/NSURL+URLQueryBuilder.h>
 
