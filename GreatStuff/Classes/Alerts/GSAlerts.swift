937e17e0afab83d8c5174c7e20339a91d37d0669
Yaroslav Arsenkin <majesticy@gmail.com>
2016-01-11 12:40:21 +0100

    Converts the Alerts class into struct and hides initializer.

--- GreatStuff/Classes/Alerts/GSAlerts.swift
+++ GreatStuff/Classes/Alerts/GSAlerts.swift
@@ -8,10 +8,27 @@
 //
 
 import UIKit
+import WatchKit
 
-public class GSAlerts {
+public struct GSAlerts {
     
-    public class func errorAlertWithMessage(message: String, title: String = "Error") -> UIAlertController {
+    private init() {}
+    
+    public static func informationalAlertWithMessage(message: String, title: String = "") -> UIAlertController {
+        return alertWithTitle(title, message: message)
+    }
+    
+    public static func errorAlertWithMessage(message: String, title: String = "Error") -> UIAlertController {
+        return alertWithTitle(title, message: message)
+    }
+
+}
+
+// MARK: - Private
+
+extension GSAlerts {
+    
+    private static func alertWithTitle(title: String, message: String) -> UIAlertController {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
         let OKAction = UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in
             alert.dismissViewControllerAnimated(true, completion: nil)
