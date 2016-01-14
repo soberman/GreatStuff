aebd80512867a6c5960da3084d96dc10b74633e6
Yaroslav Arsenkin <majesticy@gmail.com>
2016-01-14 22:37:10 +0100

    Adds rework in alerts.

--- GreatStuff/Classes/Alerts/GSAlerts.swift
+++ GreatStuff/Classes/Alerts/GSAlerts.swift
@@ -14,20 +14,6 @@ public struct GSAlerts {
     private init() {}
     
     public static func informationalAlertWithMessage(message: String, title: String = "") -> UIAlertController {
-        return alertWithTitle(title, message: message)
-    }
-    
-    public static func errorAlertWithMessage(message: String, title: String = "Error") -> UIAlertController {
-        return alertWithTitle(title, message: message)
-    }
-
-}
-
-// MARK: - Private
-
-extension GSAlerts {
-    
-    private static func alertWithTitle(title: String, message: String) -> UIAlertController {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
         let OKAction = UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in
             alert.dismissViewControllerAnimated(true, completion: nil)
@@ -37,4 +23,14 @@ extension GSAlerts {
         return alert
     }
     
+    public static func closedQuestionAlertWithMessage(message: String, title: String = "", yesHandler: ((UIAlertAction) -> Void)?, noHandler: ((UIAlertAction) -> Void)?) -> UIAlertController {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        let YESAction = UIAlertAction(title: "Yes", style: .Default, handler: yesHandler)
+        let NOAction = UIAlertAction(title: "No", style: .Cancel, handler: noHandler)
+        alert.addAction(YESAction)
+        alert.addAction(NOAction)
+        
+        return alert
+    }
+
 }
