0968edca3ba201170eb83bc77df508b11641925d
Yaroslav Arsenkin <majesticy@gmail.com>
2016-01-17 19:45:12 +0100

    Makes the new UIColor convenience method as static.

--- GreatStuff/Extensions/UIColor+GreatStuff.swift
+++ GreatStuff/Extensions/UIColor+GreatStuff.swift
@@ -10,7 +10,7 @@ import UIKit
 
 public extension UIColor {
     
-    public func gs_colorWithRGB(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) -> UIColor {
+    public class func gs_colorWithRGB(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) -> UIColor {
         return UIColor(red: red/255.0, green: green/255.0, blue: blue/255.0, alpha: alpha)
     }
     
