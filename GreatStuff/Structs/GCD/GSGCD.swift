cd556a803937f781785202d108ae149052bba53d
Yaroslav Arsenkin <majesticy@gmail.com>
2016-02-12 23:54:39 +0100

    Adds 'GSLoger' struct

---
+++ GreatStuff/Structs/GCD/GSGCD.swift
@@ -0,0 +1,9 @@
+//
+//  GSGCD.swift
+//  GreatStuff
+//
+//  Created by Yaroslav Arsenkin on 12.02.16.
+//  Copyright © 2016 Iaroslav Arsenkin. All rights reserved.
+//
+
+import Foundation
---
+++ GreatStuff/Structs/Loger/GSLoger.swift
@@ -0,0 +1,40 @@
+//
+//  GSLoger.swift
+//  GreatStuff
+//
+//  Created by Yaroslav Arsenkin on 12.02.16.
+//  Copyright © 2016 Iaroslav Arsenkin. All rights reserved.
+//
+
+import Foundation
+
+public struct GSLoger {
+    
+    public static var formattingStyle: String = "\n- __LINE__, `__METHOD__` in __FILE__:"
+    
+    public static func log(string: String, file: NSString = __FILE__, method: String = __FUNCTION__, line: Int = __LINE__) {
+        log(string, args: [], file: file, method: method, line: line)
+    }
+    
+    public static func log(string: String, args: [CVarArgType], file: NSString = __FILE__, method: String = __FUNCTION__, line: Int = __LINE__) {
+        let log = String(format: string, arguments: args)
+        let format = generateFormatString(file, method: method, line: line) + "\n   \(log)"
+        
+        print(format)
+    }
+    
+}
+
+private extension GSLoger {
+    
+    static func generateFormatString(file: NSString = __FILE__, method: String = __FUNCTION__, line: Int = __LINE__) -> String {
+        let fileTruncated = file.lastPathComponent
+        
+        let formatWithLineNumber = GSLoger.formattingStyle.stringByReplacingOccurrencesOfString("__LINE__", withString: "\(line)")
+        let formatWithMethod = formatWithLineNumber.stringByReplacingOccurrencesOfString("__METHOD__", withString: method)
+        let formatWithFile = formatWithMethod.stringByReplacingOccurrencesOfString("__FILE__", withString: fileTruncated)
+        
+        return formatWithFile
+    }
+    
+}
