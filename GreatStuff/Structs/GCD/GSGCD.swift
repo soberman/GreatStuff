96989f6a3bf129eec6416c64e04c2dada0edc59f
Yaroslav Arsenkin <majesticy@gmail.com>
2016-02-13 00:27:33 +0100

    Adds 'GSGCD' wrapper for the dispatch_after method.

--- GreatStuff/Structs/GCD/GSGCD.swift
+++ GreatStuff/Structs/GCD/GSGCD.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+
+public struct GSGCD {
+    
+    /**
+         Dispatches provided block after specified amount of secs.
+     
+         - Note: in case _queue_ is not provided, then _dispatch_get_main_queue()_ is used by default.
+     */
+    public static func dispatchAfter(sec: UInt64, queue: dispatch_queue_t = dispatch_get_main_queue(), block: () -> Void) {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(sec * NSEC_PER_SEC)), queue, block)
+    }
+    
+}
