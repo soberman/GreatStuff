081db03130aa137631dacec4f4d41afe666621a7
Yaroslav Arsenkin <majesticy@gmail.com>
2016-02-13 19:25:34 +0100

    Change the GSLog to now be as a separate function, instead of being a struct with its methods.

--- GreatStuff/Structs/Loger/GSLoger.swift
+++ GreatStuff/Structs/Loger/GSLoger.swift
@@ -8,41 +8,37 @@
 
 import Foundation
 
-public struct GSLoger {
-    
-    /**
-        Formatting style of the log output. Default:
-        
-            "\n- __LINE__, `__METHOD__` in __FILE__:"
-     
-        - Note: You should use \_\_LINE\_\_ or any other formating placeholder exactly like in example.
-     
-     */
-    public static var formattingStyle: String = "\n- __LINE__, `__METHOD__` in __FILE__:"
-    
-    public static func log(string: String, file: NSString = __FILE__, method: String = __FUNCTION__, line: Int = __LINE__) {
-        log(string, args: [], file: file, method: method, line: line)
-    }
-    
-    public static func log(string: String, args: [CVarArgType], file: NSString = __FILE__, method: String = __FUNCTION__, line: Int = __LINE__) {
-        let log = String(format: string, arguments: args)
-        let format = generateFormatString(file, method: method, line: line) + "\n   \(log)"
-        
-        print(format)
-    }
-    
+/**
+Formatting style of the log output. Default:
+
+"\n- __LINE__, `__METHOD__` in __FILE__:"
+
+- Note: You should use \_\_LINE\_\_ or any other formating placeholder exactly like in example.
+
+*/
+public var GSLogFormattingStyle: String = "\n- __LINE__, `__METHOD__` in __FILE__:"
+
+public func GSLog(string: String, file: NSString = __FILE__, method: String = __FUNCTION__, line: Int = __LINE__) {
+    GSLog(string, args: [], file: file, method: method, line: line)
 }
 
-private extension GSLoger {
+public func GSLog(string: String, args: [CVarArgType], file: NSString = __FILE__, method: String = __FUNCTION__, line: Int = __LINE__) {
+    let log = String(format: string, arguments: args)
+    let format = gs_generateFormatString(file, method: method, line: line) + "\n   \(log)"
     
-    static func generateFormatString(file: NSString = __FILE__, method: String = __FUNCTION__, line: Int = __LINE__) -> String {
-        let fileTruncated = file.lastPathComponent
-        
-        let formatWithLineNumber = GSLoger.formattingStyle.stringByReplacingOccurrencesOfString("__LINE__", withString: "\(line)")
-        let formatWithMethod = formatWithLineNumber.stringByReplacingOccurrencesOfString("__METHOD__", withString: method)
-        let formatWithFile = formatWithMethod.stringByReplacingOccurrencesOfString("__FILE__", withString: fileTruncated)
-        
-        return formatWithFile
-    }
+    print(format)
+}
+
+
+// MARK: - Private Methods
+
+
+private func gs_generateFormatString(file: NSString = __FILE__, method: String = __FUNCTION__, line: Int = __LINE__) -> String {
+    let fileTruncated = file.lastPathComponent
     
+    let formatWithLineNumber = GSLogFormattingStyle.stringByReplacingOccurrencesOfString("__LINE__", withString: "\(line)")
+    let formatWithMethod = formatWithLineNumber.stringByReplacingOccurrencesOfString("__METHOD__", withString: method)
+    let formatWithFile = formatWithMethod.stringByReplacingOccurrencesOfString("__FILE__", withString: fileTruncated)
+    
+    return formatWithFile
 }
